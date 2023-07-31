621a2f146d8bf92d4c1f6f75de5712c1b1ca49e1
djordjestojanovic1993 <djordje.stojanovic1993@gmail.com>
2023-07-31 15:19:58 +0200

    events

--- public/events.js
+++ public/events.js
@@ -1,20 +1,39 @@
-// Učitavanje JSON file-a
+function showData(datas){
+  const eventTemplate = document.getElementById('event-template')
+  const eventsWrapper = document.getElementsByClassName('events-wrapper');
+  const event = eventTemplate.content.getElementById('event');
 
-fetch('../events.json')
+  
+  for(let data of datas){
+    // console.log(data.name)
+    const clone = event.cloneNode(true);
+    console.log(clone.getElementsByClassName('event-title')[0])
+    clone.getElementsByClassName('event-title')[0].innerText = data.name;
+    clone.getElementsByClassName('event-location-p')[0].innerText = data.location;
+    clone.getElementsByClassName('event-date-p')[0].innerText = data.date;
+    clone.getElementsByClassName('event-time-p')[0].innerText = data.time;
+
+    eventsWrapper[0].appendChild(clone);
+  }
+}
+// Učitavanje JSON file-a
+function getDatasFromDB(){
+  fetch('../events.json')
   .then(response => response.json())
   .then(data => {
     // Ovde možete raditi sa podacima koji su učitani iz JSON file-a
     // console.log(data);
-    
+    showData(data);
 
-    console.log(data[0].name)
     // Na primer, prikazivanje podataka u HTML dokumentu
     const studentsList = document.getElementById('students-list');
 
-    data.students.forEach(student => {
-      const listItem = document.createElement('li');
-      listItem.textContent = `Name: ${student.name}, Age: ${student.age}, Major: ${student.major}`;
-    //   studentsList.appendChild(listItem);
-    });
+    // data.students.forEach(student => {
+    //   const listItem = document.createElement('li');
+    //   listItem.textContent = `Name: ${student.name}, Age: ${student.age}, Major: ${student.major}`;
+    // //   studentsList.appendChild(listItem);
+    // });
   })
   .catch(error => console.error('Greška prilikom učitavanja JSON file-a:', error));
+}
+getDatasFromDB();
