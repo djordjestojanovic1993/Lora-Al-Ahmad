f8e59b848d83b100389156668860f5135bbcddb6
djordjestojanovic1993 <djordje.stojanovic1993@gmail.com>
2023-07-31 07:55:51 +0200

    slike videi

--- public/index.js
+++ public/index.js
@@ -1,48 +1,27 @@
-let player; // Promenljiva za čuvanje YouTube player objekta
+var videoObserver = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(function(entry) {
+    if (entry.isIntersecting) {
+      var iframe = entry.target.querySelector("iframe");
+      var videoUrl = "https://www.youtube.com/embed/YOUR_YOUTUBE_VIDEO_ID?autoplay=1";
+      iframe.setAttribute("src", videoUrl);
+    } else {
+      var iframe = entry.target.querySelector("iframe");
+      iframe.setAttribute("src", "");
+    }
+  });
+}, { threshold: 0.5 });
 
-  // Funkcija koja će se pozvati kada div postane vidljiv
-  function handleIntersection(entries, observer) {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        // Pustite video kada div postane vidljiv
-        player = new YT.Player('player', {
-          videoId: 'liJVSwOiiwg', // Zamijenite ovo sa ID-om YouTube videa koji želite reproducirati
-          playerVars: {
-            autoplay: 1 // Pustite video automatski kada se učita
-          }
-        });
+var videoContainer = document.getElementById("video-container");
+videoObserver.observe(videoContainer);
 
-        // Isključite promatranje nakon prvog puštanja videa
-        observer.disconnect();
-      } else {
-        // Ako div postane nevidljiv, zaustavite video
-        if (player && typeof player.pauseVideo === 'function') {
-          player.pauseVideo();
-        }
-      }
-    });
-  }
+// Dodajemo event listener na učitavanje stranice kako bismo ručno pokrenuli video
+window.addEventListener("load", function() {
+  var iframe = document.getElementById("youtube-video");
+  var videoUrl = "liJVSwOiiwg";
+  iframe.setAttribute("src", videoUrl);
+});
 
-  // Kreirajte Intersection Observer
-  const options = {
-    root: null, // Koristi viewport kao root element za provjeru vidljivosti
-    rootMargin: '0px',
-    threshold: 0.5 // Proveri kada je 50% elementa vidljivo
-  };
-
-  const videoObserver = new IntersectionObserver(handleIntersection, options);
-
-  // Pratite vidljivost div-a sa klasom .video-container
-  const videoContainer = document.querySelector('.video-container');
-  // videoObserver.observe(videoContainer);
-
-  // Učitajte YouTube JavaScript API skriptu
-  const tag = document.createElement('script');
-  tag.src = 'https://www.youtube.com/iframe_api';
-  const firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-  function playVideo(){
+  function playVideoManually(){
     let videos = document.getElementsByClassName('video');
     const videoPlayWindowCover = document.getElementsByClassName('video-play-window-cover');
     const videoPlayWindow = document.getElementsByClassName('video-play-window');
@@ -53,8 +32,36 @@ let player; // Promenljiva za čuvanje YouTube player objekta
         videoPlayWindowCover[0].addEventListener('click', (e)=>{
           videoPlayWindowCover[0].classList.add('hidden')
           videoPlayWindow[0].classList.add("hidden");
+          // video.pause();
+          stopVideo();
         })
       })
     }
   }
-  playVideo();
+  playVideoManually();
+  
+  function stopVideo() {
+    var iframe = document.getElementsByClassName("vide-iframe");
+    console.log(iframe);
+    var temp = iframe[0].src;
+    iframe[0].src = temp;
+  }
+
+  function showPicture(){
+    let pictures = document.getElementsByClassName('picture-img');
+    const pictureWindowCover = document.getElementsByClassName('picture-window-cover');
+    const pictureWindow = document.getElementsByClassName('picture-window');
+    const pictureWindowImg = document.getElementById('picture-window-img');
+    for(const picture of pictures){
+      picture.addEventListener('click', (e)=>{
+        pictureWindowCover[0].classList.remove("hidden");
+        pictureWindow[0].classList.remove("hidden");
+        pictureWindowImg.src = picture.src;
+        pictureWindowCover[0].addEventListener('click', (e)=>{
+          pictureWindowCover[0].classList.add('hidden')
+          pictureWindow[0].classList.add("hidden");
+        })
+      })
+    }
+  }
+  showPicture();
