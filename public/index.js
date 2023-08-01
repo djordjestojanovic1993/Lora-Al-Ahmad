bc2e0313f0f53610706964f1a3bb96ec3bde80e3
djordjestojanovic1993 <djordje.stojanovic1993@gmail.com>
2023-08-01 08:24:50 +0200

    nav res

--- public/index.js
+++ public/index.js
@@ -65,3 +65,54 @@ window.addEventListener("load", function() {
     }
   }
   showPicture();
+
+  function hamburger(){
+    const line1 = document.getElementById('line1');
+    const line2 = document.getElementById('line2');
+    const line3 = document.getElementById('line3');
+    const hamburger = document.getElementById('hamburger');
+    const list = document.querySelector('nav ul');
+    const nav = document.querySelector('nav');
+
+
+    const openList = function(){
+        line1.style.transform = "rotate(45deg)";
+        line2.style.transform = "scaleY(0)";
+        line3.style.transform = "rotate(-45deg)";
+        list.style.display = "flex";
+        if(!nav.classList.contains('scrolled')){
+            nav.classList.add('scrolled');
+            addedScrolled = true;
+        }
+        clicked = true;
+    }
+    const closeList = function(){
+        line1.style.transform = "rotate(0deg)";
+        line2.style.transform = "scaleY(1)";
+        line3.style.transform = "rotate(0deg)";
+        list.style.display = "none";
+        if(addedScrolled){
+            nav.classList.remove('scrolled');
+            addedScrolled = false;
+        }
+        clicked = false;
+    }
+
+    let addedScrolled = false;
+    let clicked = false;
+
+    hamburger.addEventListener('click', function(){
+        if(clicked === false){
+            openList();
+        }else{
+            closeList();
+        }
+    });
+
+    list.addEventListener('click', function(){
+        if(clicked === true)
+            closeList();
+    });
+
+}
+hamburger();
