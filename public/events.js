3bf31cc9835e1ed5de72a42a899058f5aef05bdf
djordjestojanovic1993 <djordje.stojanovic1993@gmail.com>
2023-08-14 12:27:10 +0200

    n

--- public/events.js
+++ public/events.js
@@ -44,29 +44,6 @@ async function showData(events){
 // showData();
 
 
-
-
-const eventsContainer = document.getElementById('events-wrapper');
-
-async function createListOfEvents(){
-  const Event = function(name, location, date, link, when){
-    this.name = name;
-    this.location = location;
-    this.date = date;
-    this.link = link;
-    this.when = when;
-  }
-  
-  let datas = await readDataFromDB();
-  const events = [];
-  for( data of datas){
-    events.push(new Event(data.name, data.location, data.date, "", data.when));
-  }
-  sortEvents(events);
-  showData(events);
-}
-createListOfEvents();
-
 // sortiramo objekte dogadjaja u nizu prema datumu
 function sortEvents(events){
   events.sort(function(ev1, ev2){
@@ -99,4 +76,82 @@ function sortEvents(events){
     }
   });
   return events;
+}
+
+
+const eventsContainer = document.getElementById('events-wrapper');
+
+async function createListOfEvents(){
+  const Event = function(name, location, date, link, when){
+    this.name = name;
+    this.location = location;
+    this.date = date;
+    this.link = link;
+    this.when = when;
+  }
+  
+  let datas = await readDataFromDB();
+  const events = [];
+  for( data of datas){
+    events.push(new Event(data.name, data.location, data.date, "", data.when));
+  }
+  sortEvents(events);
+  showData(events);
+  let counter = numberOfPassedEvents(events);
+  console.log(counter)
+}
+createListOfEvents();
+
+
+function numberOfPassedEvents(events){
+  let today = new Date();
+
+  let currentDay = today.getDate();
+  let currentMonth = today.getMonth() + 1;
+  let currentYear = today.getFullYear();
+
+  let counter = 0;
+
+  const porediDvaBroja = function(a, b){
+      if(a>b){
+          return 1;
+      }else if(a < b){
+          return -1;
+      }else{
+          return 0;
+      }
+  }
+
+  //proveravamo koliko dogadjaja je proslo u odnosu na trenutno vreme
+  for(const event of events){
+    let day = Number.parseInt(event.when.substr(0, 2));
+    let mon = Number.parseInt(event.when.substr(2, 2));
+    let year = Number.parseInt(event.when.substr(4, 4));
+    const y = porediDvaBroja(currentYear, year);
+    if(y === 0){
+        const m = porediDvaBroja(currentMonth, mon);
+        if(m === 0){
+            const d = porediDvaBroja(currentDay, day);
+            if(d === 1){
+                counter++;
+            }
+        }else if(m === 1){
+            counter++;
+        }
+    }else if(y === 1){
+        counter++;
+    }
+  }
+  const allEvents = eventsContainer.children;
+for(let i = 1; i < counter; i++){
+    const deca = allEvents[i];
+    console.log(allEvents[i])
+    
+    const note = document.createElement('p');
+    const noteText = document.createTextNode('This event has ended');
+    note.appendChild(noteText);
+
+    allEvents[i].replaceChild(note, deca);
+} 
+  return counter;
 }
